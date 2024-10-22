dd7274edfc176c291f3fbdf4daa025f0eeb68acd
Camila <camsan26@bergen.org>
2024-10-22 10:27:17 -0400

    10/22/24 - Registering a User Form

--- myproject/users/views.py
+++ myproject/users/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
 def register(request):
+    # check if the form is submitted
+    if request.method == "POST":
+        form = UserCreationForm(request.POST) # submitted with data
+        # validate the form
+        if form.is_valid():
+            form.save()
+            return redirect("posts:list")
+    else: # get method
+        form = UserCreationForm()
     context = {
         'active_link': 'register',
+        'form': form
     }
     return render(request, "users/register.html", context)
